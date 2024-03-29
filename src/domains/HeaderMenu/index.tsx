260a94a512cfff239b94a884fface24fd94f59fa
eomttt <hyunt0413@gmail.com>
2024-03-30 00:33:36 +0900

    fix: Style merge 방법 수정

--- src/domains/HeaderMenu/index.tsx
+++ src/domains/HeaderMenu/index.tsx
@@ -1,13 +1,14 @@
 import { Flex } from '@/packages/design-packages/components/layout/Flex';
+import { css } from '../../../styled-system/css';
 
 const HeaderMenu = () => {
   return (
     <Flex
       align="center"
-      css={{
+      className={css({
         height: '100%',
         px: 10,
-      }}
+      })}
     >
       Inbox
     </Flex>
