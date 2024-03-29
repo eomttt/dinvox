f8ef4ff84ed7813a749d30e641a86d9913ae6d6e
eomttt <hyunt0413@gmail.com>
2024-03-29 23:39:10 +0900

    fix: Typography component

---
+++ src/packages/design-packages/components/typography/Typography.tsx
@@ -0,0 +1,30 @@
+import { ComponentPropsWithoutRef } from 'react';
+import { cva, cx } from '../../../../../styled-system/css';
+
+const typographyRecipe = cva({
+  base: {
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 1.5,
+  },
+});
+
+interface Props extends ComponentPropsWithoutRef<'span'> {}
+
+const Typography = ({ children, className, ...props }: Props) => {
+  return (
+    <span
+      className={cx(
+        // using the list recipe
+        typographyRecipe(),
+        // adding style overrides (external)
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
+
+export { Typography };
